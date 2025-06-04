5bfd261a812fb736bbaac320eb752d21080d6112
Yagya Soni <yagyasony@gmail.com>
2025-06-04 19:47:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import StudentApp from './StudentApp';
 import './App.css';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io('https://polling-system-backend-wqqh.onrender.com');
 
 function App() {
   const [persona, setPersona] = useState(null);
